2228e9419ce362135955f58d892b9f152f002957
melosomelo <mateusmelornrn@gmail.com>
2023-04-10 11:16:09 -0300

    improve error message formatting

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,4 @@
-import { Request, Response, RequireAtLeastOne } from "../types";
+import { Request, Response } from "../types";
 import UserService from "../services/user";
 import APIError from "../errors/APIError";
 
--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,15 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import { validationResult } from "express-validator";
+import { validationResult, ValidationError } from "express-validator";
 import APIError from "../errors/APIError";
 
 function validationMiddleware(req: Request, _: Response, next: NextFunction) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     let firstError = errors.array()[0];
-    throw new APIError(
-      `Invalid value for ${firstError.param}: ${firstError.msg}`,
-      400
-    );
+    // Deal with cases of nested errors so as to not return a generic message.
+    while (firstError.nestedErrors) {
+      firstError = firstError.nestedErrors[0] as ValidationError;
+    }
+    // No parameter specifically. Probably entire body validation.
+    let msg;
+    if (firstError.param === "") {
+      msg = `Invalid request body: ${firstError.msg}`;
+    } else {
+      msg = `Invalid value for ${firstError.param}: ${firstError.msg}`;
+    }
+    throw new APIError(msg, 400);
   }
   next();
 }
